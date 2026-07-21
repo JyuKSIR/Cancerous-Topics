4ef16e663c78fa6832ffbfe37895b9b1864fb937
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2026-07-22 02:48:57 +0600

    Create Bit Masking.cpp

---
+++ Bit Things/Bit Masking.cpp
@@ -0,0 +1,24 @@
+// iterate through all possible subsets
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  cin.tie(0)->sync_with_stdio(0);
+  
+  ll n; cin >> n;
+  ll a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  ll sz = (1 << n);
+  for (int i = 0; i < sz; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i & (1 << j)) {
+        // if the bit is set
+      } else {
+        // if the bit is not set
+      }
+    }
+  }
+}
