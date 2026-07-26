fbd839d8ae0445be2f9ba5d80dcb044b9e7eb1be
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2026-07-27 00:12:41 +0600

    Create Divisors.cpp

---
+++ Number Theory/Divisors.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+
+  int n, ct = 0, sum = 0;
+  cin >> n;
+  for (int i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      cout << i << " " << n / i << "\n";
+      ct++;
+      sum += i;
+      if (n / i != i) {
+        ct++;
+        sum += n / i;
+      }
+    }
+  }
+  cout << "count of divisors: " << ct << " Sum: " << sum << "\n";
+}
